19fc5fe641d8c503e333d158864b7ac13925fdf1
Theobald <turtleman4763@gmail.com>
2025-05-15 23:21:57 -0500

    1.21.4 + disabling unnecessary LivingEntityRendererMixin + removing unused flashback stuff

--- src/client/java/com/theobald/mixin/LivingEntityRendererMixin.java
+++ src/client/java/com/theobald/mixin/LivingEntityRendererMixin.java
@@ -15,13 +15,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(LivingEntityRenderer.class)
 public abstract class LivingEntityRendererMixin<T extends LivingEntity> {
 
-    @Unique
-    ClientPlayerEntity clientPlayerEntity = MinecraftClient.getInstance().player;
-
-    @Inject(method = "hasLabel", at = @At("HEAD"), cancellable = true)
-    private void forceRenderNametag(T entity, CallbackInfoReturnable<Boolean> cir) {
-        if (entity instanceof ClientPlayerEntity && FightCameraClient.active) {
-            cir.setReturnValue(true); // Always show the name tag for the local player
-        }
-    }
+//    @Inject(method = "hasLabel", at = @At("HEAD"), cancellable = true)
+//    private void forceRenderNametag(T entity, CallbackInfoReturnable<Boolean> cir) {
+//        if (entity instanceof ClientPlayerEntity && FightCameraClient.active) {
+//            cir.setReturnValue(true); // Always show the name tag for the local player
+//        }
+//    }
 }
